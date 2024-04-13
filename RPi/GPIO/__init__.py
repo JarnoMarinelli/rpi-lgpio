8ee7a5edfa9e85e0939a0bd8645cd9d64471262e
Jarno Marinelli <39005969+JarnoMarinelli@users.noreply.github.com>
2024-04-13 19:16:27 +0200

    Update __init__.py

--- RPi/GPIO/__init__.py
+++ RPi/GPIO/__init__.py
@@ -17,7 +17,6 @@ try:
     # (jammy) and 0.2.0.0 (kinetic)
     lgpio.SET_PULL_NONE
 except AttributeError:
-    lgpio.SET_PULL_NONE = lgpio.SET_BIAS_DISABLE
     lgpio.SET_PULL_UP = lgpio.SET_BIAS_PULL_UP
     lgpio.SET_PULL_DOWN = lgpio.SET_BIAS_PULL_DOWN
 
